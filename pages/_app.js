ce73bec8bca98985c34c52defb0a859a2f120487
Samuel Ko <koshunyin@gmail.com>
2022-03-24 21:24:52 +1100

    add base font size

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,8 @@ const GlobalStyle = createGlobalStyle`
 	body {
 		padding: 0;
 		margin: 0;
+		font-size: 16px;
+		font-weight: normal;
 		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
 			Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 		background-color: ${props => props.theme.color.background};
