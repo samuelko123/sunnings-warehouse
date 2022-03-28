c962185031afdf4df7e632738e2260b5c0161864
Samuel Ko <koshunyin@gmail.com>
2022-03-28 20:21:29 +1100

    improve layout

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -4,22 +4,19 @@ import Link from 'next/link'
 import styled from 'styled-components'
 
 const Card = styled.article`
+	position: relative;
+
 	display: flex;
 	flex-direction: column;
 	flex-wrap: nowrap;
     justify-content: flex-start;
     align-content: center;
 
-	height: 17rem;
-
-	position: relative;
-
     background-color: ${props => props.theme.color.surface};
     color: ${props => props.theme.color.onSurface};
 `
 
 const CardImage = styled.img`
-	margin: auto;
 	width: 100%;
 `
 
@@ -32,32 +29,43 @@ const CardTitle = styled.span`
 	color: ${props => props.theme.color.primary};
 `
 
-const CardFooter = styled(Box)`
+const CardHeader = styled(Box)`
+	position: absolute;
+	top: 0;
+	z-index: 2;
+
+	width: 100%;
+	height: auto;
+	max-height: 2.5rem;
+
+	background-color: ${props => props.theme.color.surface};
+	overflow: hidden;
+
 	${Card}:hover & {
-		position: absolute;
-		bottom: 0;
-		background-color: ${props => props.theme.color.surface};
+		max-height: 10rem;
 	}
 
-	transition: all 0.5s;
+	transition: max-height 1s ease;
 `
 
 const CardDesc = styled.p`
-	display: none;
-	${Card}:hover & {
-		display: -webkit-box;
-	}
-   	-webkit-line-clamp: 4;
-   	-webkit-box-orient: vertical;
-   	overflow: hidden;
-   	text-overflow: ellipsis;
-	transition: all 0.5s;
-
-	margin: 0;
-
+	margin-top: 0.5rem;
+	margin-bottom: 0;
 	font-family: "Helvetica Neue", sans-serif;
     font-size: 0.9rem;
     line-height: 1.5rem;
+
+	display: -webkit-box;
+	overflow: hidden;
+   	text-overflow: ellipsis;
+	-webkit-line-clamp: 3;
+   	-webkit-box-orient: vertical;
+`
+
+const Figure = styled.figure`
+	position: relative;
+	top: 2.5rem;
+	z-index: 1;
 `
 
 export const ArticleCard = (props) => {
@@ -68,16 +76,19 @@ export const ArticleCard = (props) => {
 		desc,
 	} = props
 
-	const [showDesc, setShowDesc] = React.useState(false)
-
 	return (
 		<Link href='/' passHref={true}>
 			<a>
-				<Card
-					onMouseEnter={() => setShowDesc(true)}
-					onMouseLeave={() => setShowDesc(false)}
-				>
-					<figure>
+				<Card>
+					<CardHeader p={1}>
+						<CardTitle>
+							{name}
+						</CardTitle>
+						<CardDesc>
+							{desc}
+						</CardDesc>
+					</CardHeader>
+					<Figure>
 						<picture>
 							<source
 								media='(min-width: 0px)'
@@ -88,16 +99,7 @@ export const ArticleCard = (props) => {
 								src={picLink2}
 							/>
 						</picture>
-					</figure>
-
-					<CardFooter p={1} expand={showDesc}>
-						<CardTitle>
-							{name}
-						</CardTitle>
-						<CardDesc show={showDesc}>
-							{desc}
-						</CardDesc>
-					</CardFooter>
+					</Figure>
 				</Card>
 			</a>
 		</Link>
