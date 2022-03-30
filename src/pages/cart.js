9a8cc0c152f7e42f99e90b6f9cca34c52a09d88d
Samuel Ko <koshunyin@gmail.com>
2022-03-30 20:06:01 +1100

    update cart page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,12 +1,51 @@
+import styled from 'styled-components'
 import { FlexBox } from '../components/Flexbox'
+import { ShopButton } from '../components/Button'
+
+const Container = styled(FlexBox)`
+	margin: 2.5rem auto;
+    text-align: center;
+
+	@media (min-width: 48rem) {
+		width: 24rem;
+	}
+`
+
+const Title = styled.h1`
+	margin: 0;
+    font-family: Futura, "Helvetica Neue", sans-serif;
+    font-size: 2rem;
+    line-height: 1.5;
+`
+
+const Paragraph = styled.p`
+	margin: 0;
+    font-family: "Helvetica Neue", sans-serif;
+    font-size: 1rem;
+    line-height: 1.5;
+`
 
 export default function Cart() {
 	return (
-		<FlexBox
+		<Container
+			flexDirection='column'
 			justifyContent='center'
 			alignItems='center'
+			gap={4}
 		>
-			<h1>WIP - Cart</h1>
-		</FlexBox>
+			<img
+				src='/img/trolley.svg'
+				alt='Empty cart Icon'
+			/>
+			<Title>
+				Your cart is empty!
+			</Title>
+			<Paragraph>
+				To add items to your cart and continue shopping, check out our wide range of products that are now available to buy online
+			</Paragraph>
+			<ShopButton>
+				Continue Shopping
+			</ShopButton>
+		</Container>
 	)
 }
